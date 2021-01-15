48a6f257927931584506b308cc779f56def477cc
Keila Beatriz <keila_BvS@outlook.com>
2021-01-14 21:32:35 -0300

    adicionando teste para doacao com taxa

--- tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
+++ tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
@@ -37,6 +37,22 @@ namespace Vaquinha.Unit.Tests.DomainTests
             doacao.ErrorMessages.Should().BeEmpty();
         }
 
+        [Fact] // caso de teste adicionado
+        [Trait("Doacao", "Doacao_UsuarioAceitaPagarComTaxa_DoacaoValida")] //organizar os testes
+        public void Doacao_UsuarioAceitaPagarComTaxa_DoacaoValida()
+        {
+            // Arrange
+            var doacao = _doacaoFixture.DoacaoValida(false, 5, false, true);
+            doacao.AdicionarEnderecoCobranca(_enderecoFixture.EnderecoValido());
+            doacao.AdicionarFormaPagamento(_cartaoCreditoFixture.CartaoCreditoValido());
+
+            // Act
+            var valido = doacao.Valido();
+
+            // Assert
+            doacao.Valor.Should().Be(6, because: "valor com taxa de 20%");
+        }
+
         [Fact]
         [Trait("Doacao", "Doacao_DadosPessoaisInvalidos_DoacaoInvalida")]
         public void Doacao_DadosPessoaisInvalidos_DoacaoInvalida()
